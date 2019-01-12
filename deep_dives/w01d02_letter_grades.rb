fe2a98064bfd3eb245b455d10a14ec58bc6f3fdf
markhilkert <mark.hilkert@gmail.com>
2019-01-12 17:43:01 -0600

    finished drive_thru, will add bonuses later

---
+++ deep_dives/w01d02_letter_grades.rb
@@ -0,0 +1,17 @@
+# Commit 1 - Research
+
+
+
+# Commit 1 - Initial Solution
+
+def calculate_letter_grade()
+  # write code here  
+end
+
+# Commit 2 - Refactor Solution
+
+
+
+# Commit 3 - Write Runner Code / Tests
+
+
