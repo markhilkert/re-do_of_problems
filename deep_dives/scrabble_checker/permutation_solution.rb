0ca5cc06f6c4f46ba889000022adb6e00310058b
markhilkert <mark.hilkert@gmail.com>
2019-01-17 21:12:42 -0600

    added solution for superhero class

---
+++ deep_dives/scrabble_checker/permutation_solution.rb
@@ -0,0 +1,34 @@
+
+
+# Commit 1 - Initial Solution
+
+def permutation_checker(base_word, possible_permutation)
+  base_word = base_word.chars
+  possible_permutation = possible_permutation.chars
+
+  possible_permutation.each do |letter|
+    base_word_index = base_word.index(letter)
+
+    if base_word.include?(letter)
+      base_word.delete_at(base_word_index)
+    else
+      return false
+    end
+  end
+  return true
+end
+
+# Commit 4 - Refactored Solution
+
+
+
+# Bonus 1 - Second Method
+
+ # add bonus method here
+
+# Commit 3 & 5 Runner Code
+
+p permutation_checker("caliber", "eclair")
+p permutation_checker("caliber", "bob")
+p permutation_checker("caliber", "calberr")
+
